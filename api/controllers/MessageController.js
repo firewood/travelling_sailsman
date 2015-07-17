d111990ff9ee27e1442b31b5e534e1eb375b7aa5
firewood <karamaki@gmail.com>
2015-07-17 10:37:51 +0900

    impl MessageController

--- api/controllers/MessageController.js
+++ api/controllers/MessageController.js
@@ -6,6 +6,30 @@
  */
 
 module.exports = {
-	
+
+  find: function(req, res) {
+    Message.find().exec(function(err, messages) {
+      console.log('Message::find');
+
+      Message.subscribe(req, messages);
+      if (req.options.autoWatch) {
+        Message.watch(req);
+      }
+
+      res.json(messages);
+    });
+  },
+
+  create: function(req, res) {
+    var text = req.param('text');
+    Message.create({ text: text }).exec(function(err, message) {
+      console.log('Message::create');
+
+      Message.publishCreate(message, !req.options.mirror && req);
+
+      res.json(message);
+    });
+  }
+
 };
 

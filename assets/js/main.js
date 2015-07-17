c63ff9323fb9d8de2b221dbf333638cf07c4b444
firewood <karamaki@gmail.com>
2015-07-17 19:54:48 +0900

    Add channel

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,26 @@
 (function() {
 
+  var currentChannel;
+
+  function appendChannel(name) {
+    $('#channels').append('<tr><td>' + name + '</td></tr>');
+  }
+
+  io.socket.get('/channel', function(channels) {
+    for (var i = 0; i < channels.length; i++) {
+      appendChannel(channels[i].name);
+    }
+  });
+
   io.socket.get('/message', function(messages) {
     for (var i = 0; i < messages.length; i++) {
       $('#chat-timeline').append('<li>' + messages[i].text + '</li>');
+    }
+  });
+
+  io.socket.on('channel', function(channel) {
+    if (channel.verb == 'created') {
+      appendChannel(channel.data.name);
     }
   });
 
@@ -24,5 +42,28 @@
       $text.val('');
     });
   });
+
+
+  $('#new-channel-button').on('click', function() {
+    var $text = $('#channel-name');
+    var name = $text.val();
+    console.log("channel name: " + name);
+
+    io.socket.post("/channel", {
+      name: name
+    }, function(res) {
+      console.log("CHANNEL");
+      console.log(res);
+
+      if (res.new) {
+        appendChannel(res.name);
+      }
+
+      currentChannel = res;
+      $('#current-channel').text("Current channel: " + res.name);
+      $text.val('');
+    });
+  });
+
 
 })();
